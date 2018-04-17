e0b2f630f752ec2b7c06186f0d2381f077f8e9e3
Miguel Duarte <gamer-go@hotmail.com>
2018-04-17 16:25:00 +0100

    Added project files

---
+++ XLPOOLsion-server/desktop/src/com/xpoolsion/server/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.xpoolsion.server.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.xpoolsion.server.XPOOLsionServer;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new XPOOLsionServer(), config);
+	}
+}
