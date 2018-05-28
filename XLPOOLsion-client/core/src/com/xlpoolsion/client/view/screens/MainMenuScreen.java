3505acae10043a1998ab01143f54602d98f3cb1e
Miguel Duarte <miguelpduarte98@gmail.com>
2018-05-28 11:43:47 +0100

    Added mock buttons for now

--- XLPOOLsion-client/core/src/com/xlpoolsion/client/view/screens/MainMenuScreen.java
+++ XLPOOLsion-client/core/src/com/xlpoolsion/client/view/screens/MainMenuScreen.java
@@ -1,5 +1,6 @@
 package com.xlpoolsion.client.view.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
@@ -15,18 +16,35 @@ public class MainMenuScreen extends StageScreen {
     }
 
     private void loadAssets() {
-        xlpooLsionClient.getAssetManager().load("badlogic.jpg", Texture.class);
-        xlpooLsionClient.getAssetManager().load("black_circle_50px.png", Texture.class);
+        xlpooLsionClient.getAssetManager().load("button_connect_up.png", Texture.class);
+        xlpooLsionClient.getAssetManager().load("button_connect_down.png", Texture.class);
+        xlpooLsionClient.getAssetManager().load("button_exit_up.png", Texture.class);
+        xlpooLsionClient.getAssetManager().load("button_exit_down.png", Texture.class);
         xlpooLsionClient.getAssetManager().finishLoading();
     }
 
     private void createGUI() {
         createConnectButton();
+        createExitButton();
+    }
+
+    private void createExitButton() {
+        Button exitButton = ButtonFactory.makeButton(
+                xlpooLsionClient, "button_exit_up.png", "button_exit_down.png", stage.getWidth() * 0.5f, stage.getHeight() * 0.5f,
+                stage.getWidth() * 0.4f, stage.getHeight() * 0.1f
+        );
+        exitButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.exit();
+            }
+        });
+        stage.addActor(exitButton);
     }
 
     private void createConnectButton() {
         Button connectButton = ButtonFactory.makeButton(
-                xlpooLsionClient, "black_circle_50px.png", "badlogic.jpg", stage.getWidth() * 0.5f, stage.getHeight() * 0.8f,
+                xlpooLsionClient, "button_connect_up.png", "button_connect_down.png", stage.getWidth() * 0.5f, stage.getHeight() * 0.8f,
                 stage.getWidth() * 0.4f, stage.getHeight() * 0.1f);
 
         connectButton.addListener(new ClickListener() {
