2bb73f8053a901621296be9f77563cc3691a6542
Miguel Duarte <miguelpduarte98@gmail.com>
2018-05-30 12:30:21 +0100

    Prepared loading of fonts

--- XLPOOLsion-server/core/src/com/xlpoolsion/server/XLPOOLsionServer.java
+++ XLPOOLsion-server/core/src/com/xlpoolsion/server/XLPOOLsionServer.java
@@ -1,11 +1,17 @@
 package com.xlpoolsion.server;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.assets.loaders.FileHandleResolver;
+import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGeneratorLoader;
+import com.badlogic.gdx.graphics.g2d.freetype.FreetypeFontLoader;
 import com.xlpoolsion.server.networking.NetworkRouter;
 import com.xlpoolsion.server.view.TextureManager;
-import com.xlpoolsion.server.view.screens.GameScreen;
 import com.xlpoolsion.server.view.screens.LobbyScreen;
 
 public class XLPOOLsionServer extends Game {
@@ -17,7 +23,16 @@ public class XLPOOLsionServer extends Game {
 	public void create () {
 		batch = new SpriteBatch();
 		assetManager = new AssetManager();
+
+		// set the loaders for the generator and the fonts themselves
+		FileHandleResolver resolver = new InternalFileHandleResolver();
+		assetManager.setLoader(FreeTypeFontGenerator.class, new FreeTypeFontGeneratorLoader(resolver));
+		assetManager.setLoader(BitmapFont.class, ".ttf", new FreetypeFontLoader(resolver));
+
 		textureManager = new TextureManager(assetManager);
+
+		//Video configs
+		Gdx.graphics.setWindowedMode(1920, 1080);
 
 		initView();
 	}
