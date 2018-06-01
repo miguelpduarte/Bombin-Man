f9e82854b74866266e7f1fe7704aa8c5fbbbe8f6
Miguel Duarte <miguelpduarte98@gmail.com>
2018-06-01 20:23:25 +0100

    Now using font instead of images to display number of powerups
    Need to remove images from loading and from assets

--- XLPOOLsion-server/core/src/com/xlpoolsion/server/view/screens/GameScreen.java
+++ XLPOOLsion-server/core/src/com/xlpoolsion/server/view/screens/GameScreen.java
@@ -3,14 +3,14 @@ package com.xlpoolsion.server.view.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.freetype.FreetypeFontLoader;
 import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
@@ -24,7 +24,6 @@ import com.xlpoolsion.server.view.entities.ViewFactory;
 
 import java.util.List;
 
-import static com.xlpoolsion.server.controller.levels.BaseLevelController.LEVEL_HEIGHT;
 import static com.xlpoolsion.server.controller.levels.BaseLevelController.LEVEL_WIDTH;
 
 public class GameScreen extends ScreenAdapter {
@@ -47,14 +46,10 @@ public class GameScreen extends ScreenAdapter {
     //Adjust according to the player height in the future
     public static final float PIXEL_TO_METER = 0.08f;
 
-    public static final float HUD_WIDTH = 160;
-    public static final float HUD_HEIGHT = 96;
-    public static final float HUD_GAP = 165;
-    public static final float SCALE_POWER = 1.5f;
-    public static final float HUD_POWER_SIZE = RadiusUpView.WIDTH / SCALE_POWER;
-
     private XLPOOLsionServer xlpooLsionServer;
     private Viewport viewport;
+
+    private BitmapFont main_size20;
 
     public GameScreen(XLPOOLsionServer xlpooLsionServer) {
         this.xlpooLsionServer = xlpooLsionServer;
@@ -74,6 +69,17 @@ public class GameScreen extends ScreenAdapter {
         ///Moved to TextureManager
         //this.xlpooLsionServer.getAssetManager().load("badlogic.jpg", Texture.class);
         //this.xlpooLsionServer.getAssetManager().finishLoading();
+        loadFont();
+    }
+
+    private void loadFont() {
+        FreetypeFontLoader.FreeTypeFontLoaderParameter candl_regular_20px = new FreetypeFontLoader.FreeTypeFontLoaderParameter();
+        candl_regular_20px.fontFileName = "fonts/Champagne & Limousines.ttf";
+        candl_regular_20px.fontParameters.size = 20;
+        xlpooLsionServer.getAssetManager().load("main_size20.ttf", BitmapFont.class, candl_regular_20px);
+        xlpooLsionServer.getAssetManager().finishLoading();
+
+        main_size20 = this.xlpooLsionServer.getAssetManager().get("main_size20.ttf", BitmapFont.class);
     }
 
     @Override
@@ -168,12 +174,18 @@ public class GameScreen extends ScreenAdapter {
         }
     }
 
+    private static final float HUD_WIDTH = 160;
+    private static final float HUD_HEIGHT = 96;
+    private static final float HUD_GAP = 165;
+    private static final float SCALE_POWER = 1.5f;
+    private static final float HUD_POWER_SIZE = RadiusUpView.WIDTH / SCALE_POWER;
+
     private void drawHUD(PlayerModel[] players) {
         for (int i = 0; i < GameController.MAX_PLAYERS; i++) {
             float hud_x = ENTITY_VIEW_X_SHIFT - (LEVEL_WIDTH / 2) / PIXEL_TO_METER + HUD_GAP / 2 + i * HUD_GAP;
             float hud_y = ENTITY_VIEW_Y_SHIFT + (LEVEL_WIDTH / 2) / PIXEL_TO_METER + HUD_HEIGHT;
 
-            if (players[i] != null) {
+            if (players[i] != null && !players[i].isDying()) {
                 Texture uiBox = xlpooLsionServer.getAssetManager().get("UI_Box0" + i + ".png");
                 Sprite uiSpriteBox = new Sprite(uiBox);
                 uiSpriteBox.setSize(HUD_WIDTH, HUD_HEIGHT);
@@ -199,45 +211,44 @@ public class GameScreen extends ScreenAdapter {
         }
     }
 
-    private void drawAmount(PlayerModel player,float hud_x,float hud_y) {
-        int speedAmount = player.getSpeedChanger();
-        int bombAmount = player.getAllowedBombsChanger();
-        int radiusAmount = player.getExplosionChanger();
+    private void drawAmount(PlayerModel player, float hud_x, float hud_y) {
+        int[] amounts = new int[] {
+                player.getSpeedChanger(), player.getAllowedBombsChanger(), player.getExplosionChanger()
+        };
 
-        Texture[] amounts = new Texture[3];
-        amounts[0] = xlpooLsionServer.getAssetManager().get(speedAmount + "Text.png");
-        amounts[1] = xlpooLsionServer.getAssetManager().get(bombAmount + "Text.png");
-        amounts[2] = xlpooLsionServer.getAssetManager().get(radiusAmount + "Text.png");
-        for(int i=0;i<amounts.length;i++){
-            Sprite nrSprite = new Sprite(amounts[i]);
-            nrSprite.setSize(HUD_POWER_SIZE/2, HUD_POWER_SIZE);
-            nrSprite.setCenter(hud_x + HUD_POWER_SIZE * 2, hud_y + i * HUD_POWER_SIZE);
-            nrSprite.setRotation(0);
-            nrSprite.draw(xlpooLsionServer.getBatch());
+        for(int i = 0; i < amounts.length; i++){
+            //nrSprite.setSize(HUD_POWER_SIZE/2, HUD_POWER_SIZE);
+            //nrSprite.setCenter(hud_x + HUD_POWER_SIZE * 2, hud_y + i * HUD_POWER_SIZE);
+            main_size20.draw(xlpooLsionServer.getBatch(), "" + amounts[i],
+                    hud_x + HUD_POWER_SIZE, hud_y + i * HUD_POWER_SIZE + HUD_POWER_SIZE/4,
+                    HUD_POWER_SIZE*2, Align.center, false);
         }
 
 
     }
 
-    private void drawPowerHud(PowerUpModel.PowerUpType power,float hud_x,float hud_y){
-        Texture powerUp = xlpooLsionServer.getAssetManager().get("PwrBombUp.png");
+    private void drawPowerHud(PowerUpModel.PowerUpType power, float hud_x, float hud_y){
+        Texture powerTex;
         switch (power){
             case BombsUp:
-                powerUp = xlpooLsionServer.getAssetManager().get("PwrBombUp.png");
+                powerTex = xlpooLsionServer.getAssetManager().get("PwrBombUp.png");
                 break;
             case SpeedUp:
-                powerUp = xlpooLsionServer.getAssetManager().get("PwrSpeedUp.png");
+                powerTex = xlpooLsionServer.getAssetManager().get("PwrSpeedUp.png");
                 break;
             case BombRadUp:
-                powerUp = xlpooLsionServer.getAssetManager().get("PwrRadiusUp.png");
+                powerTex = xlpooLsionServer.getAssetManager().get("PwrRadiusUp.png");
+                break;
+            default:
+                powerTex = xlpooLsionServer.getAssetManager().get("PwrRadiusUp.png");
                 break;
         }
-        Sprite pwrSprite = new Sprite(powerUp);
+
+        Sprite pwrSprite = new Sprite(powerTex);
         pwrSprite.setSize(HUD_POWER_SIZE, HUD_POWER_SIZE);
         pwrSprite.setCenter(hud_x + HUD_POWER_SIZE / 2, hud_y);
         pwrSprite.setRotation(0);
         pwrSprite.draw(xlpooLsionServer.getBatch());
-
     }
 
     private static final boolean usingMobile = true;
@@ -275,6 +286,7 @@ public class GameScreen extends ScreenAdapter {
 
     @Override
     public void dispose() {
+        //TODO: Move to transition to another screen
         NetworkRouter.getInstance().closeServer();
     }
 }
