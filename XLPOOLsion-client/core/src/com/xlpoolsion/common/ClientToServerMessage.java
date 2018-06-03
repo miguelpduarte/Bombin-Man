d3d0dd2bc9c1840abc690e58bf2296204fd12883
Miguel Duarte <miguelpduarte98@gmail.com>
2018-06-03 14:33:35 +0100

    Documented messages

--- XLPOOLsion-client/core/src/com/xlpoolsion/common/ClientToServerMessage.java
+++ XLPOOLsion-client/core/src/com/xlpoolsion/common/ClientToServerMessage.java
@@ -4,7 +4,13 @@ import com.badlogic.gdx.math.Vector2;
 
 import java.io.Serializable;
 
+/**
+ * Represents a message from the Client to the Server. Serializable for ease of sending and receiving.
+ */
 public class ClientToServerMessage implements Serializable {
+    /**
+     * The type of the Message
+     */
     public enum MessageType {
         CONTROLLER_SHAKE,
         PLAYER_MOVE,
@@ -13,14 +19,29 @@ public class ClientToServerMessage implements Serializable {
         ACK
     }
 
+    /**
+     * The stored message type
+     */
     public final MessageType messageType;
+    /**
+     * The stored movement direction
+     */
     public final Vector2 move_direction;
 
+    /**
+     * Builds a message with the given type and movement direction
+     * @param messageType Type of the message
+     * @param move_direction Direction of movement
+     */
     public ClientToServerMessage(MessageType messageType, Vector2 move_direction) {
         this.messageType = messageType;
         this.move_direction = move_direction;
     }
 
+    /**
+     * Builds a message without movement direction
+     * @param messageType The type of the message
+     */
     public ClientToServerMessage(MessageType messageType) {
         this(messageType, null);
     }
--- XLPOOLsion-client/core/src/com/xlpoolsion/common/ServerToClientMessage.java
+++ XLPOOLsion-client/core/src/com/xlpoolsion/common/ServerToClientMessage.java
@@ -2,7 +2,13 @@ package com.xlpoolsion.common;
 
 import java.io.Serializable;
 
+/**
+ * Represents a message from the Server to the Client. Serializable for ease of sending and receiving.
+ */
 public class ServerToClientMessage implements Serializable {
+    /**
+     * The type of the Message
+     */
     public enum MessageType {
         START_GAME,
         SERVER_FULL,
@@ -12,8 +18,15 @@ public class ServerToClientMessage implements Serializable {
         YOU_LOST
     }
 
+    /**
+     * The stored message type
+     */
     public final MessageType messageType;
 
+    /**
+     * Builds a message with the given type
+     * @param messageType Type of the message
+     */
     public ServerToClientMessage(MessageType messageType) {
         this.messageType = messageType;
     }
